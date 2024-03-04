1e2127dee7158aa9f71b86ce311ecdc80066beab
DBLukas <lukas.debacker01@gmail.com>
2024-03-04 21:35:28 +0100

    feat(fix gridlist and viewlist only button events): button events gridlist and gridview

--- src/client/components/viewActions.ts
+++ src/client/components/viewActions.ts
@@ -8,26 +8,26 @@ export class ViewActions extends HTMLElement {
     this.render();
   }
 
-  ViewAction = () => {
-    var listView = document.querySelector(".list-view");
-    var gridView = document.querySelector(".grid-view");
-    var projectsList = document.querySelector(".project-boxes");
+  rigthButton = () => {
+    var listView = this.querySelector(".left-button");
+    var gridView = this.querySelector(".right-buttton");
+
+    listView?.classList.remove("active");
+    gridView?.classList.add("active");
+  };
+
+  leftButton = () => {
+    var listView = this.querySelector(".left-button");
+    var gridView = this.querySelector(".right-buttton");
 
     gridView?.classList.remove("active");
     listView?.classList.add("active");
-    projectsList?.classList.remove("jsGridView");
-    projectsList?.classList.add("jsListView");
-
-    gridView?.classList.add("active");
-    listView?.classList.remove("active");
-    projectsList?.classList.remove("jsListView");
-    projectsList?.classList.add("jsGridView");
   };
 
   render = () => {
     render(
       html` <div class="view-actions">
-      <button @click=${this.ViewAction} class="view-btn list-view">
+      <button @click=${this.leftButton} class="view-btn left-button">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list">
           <line x1="8" y1="6" x2="21" y2="6" />
           <line x1="8" y1="12" x2="21" y2="12" />
@@ -36,7 +36,7 @@ export class ViewActions extends HTMLElement {
           <line x1="3" y1="12" x2="3.01" y2="12" />
           <line x1="3" y1="18" x2="3.01" y2="18" /></svg>
       </button>
-      <button @click=${this.ViewAction} class="view-btn grid-view active"">
+      <button @click=${this.rigthButton} class="view-btn right-buttton active"">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid">
           <rect x="3" y="3" width="7" height="7" />
           <rect x="14" y="3" width="7" height="7" />
