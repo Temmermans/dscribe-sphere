dce70d7938f6987043bccd2674e50ea5ec4ca2e3
Julie Boone <113933101+julie-boone@users.noreply.github.com>
2024-03-04 18:57:17 +0100

    feat(client): add profile icon components (#3)

---
+++ src/client/components/profileIcon.ts
@@ -0,0 +1,33 @@
+import { html, render } from "lit-html";
+
+interface User {
+  name: string;
+  picture: string;
+}
+
+export class ProfileIcon extends HTMLElement {
+  user: User;
+
+  constructor() {
+    super();
+    this.user = this.dataset.user
+      ? JSON.parse(this.dataset.user)
+      : {
+          name: "Julie",
+          picture: "https://assets.codepen.io/3306515/IMG_2025.jpg",
+        };
+  }
+
+  connectedCallback() {
+    render(this.template(), this);
+  }
+
+  template() {
+    return html`
+      <button class="profile-btn">
+        <img src=${this.user.picture} />
+        <span>${this.user.name}</span>
+      </button>
+    `;
+  }
+}
