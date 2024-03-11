ed088d74580db1899f231ec536e89b7826c6327b
PIETER CARDOEN <pcardoen@gmail.com>
2024-03-11 21:46:20 +0100

    feat(client): other navbar buttons in comment

--- src/client/main.ts
+++ src/client/main.ts
@@ -46,6 +46,8 @@ document.querySelector<HTMLDivElement>("body")!.innerHTML = `
   <div class="app-content">
     <div class="app-sidebar">
       <nav-bar-button data-image="http://www.w3.org/2000/svg" data-route="/company"></nav-bar-button>
+      <nav-bar-button data-image="http://www.w3.org/2000/svg" data-route="/user"></nav-bar-button>
+      <!--  
       <a href="" class="app-sidebar-link active">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
           <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
@@ -71,8 +73,9 @@ document.querySelector<HTMLDivElement>("body")!.innerHTML = `
           <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
         </svg>
       </a>
+      -->
     </div>
-    <div id="main"> <p>hallo</p></div>
+    <div id="main">
     <div class="projects-section">
       <div class="projects-section-header">
         <p>Projects</p>
@@ -325,6 +328,7 @@ document.querySelector<HTMLDivElement>("body")!.innerHTML = `
           </div>
         </div>
       </div>
+    </div>
     </div>
   </div>
 </div>
